30a89d698189e85534ed3e618232b4465a3e1087
shanx2 <shanshan.cao@live.nl>
2018-08-10 14:06:44 +0200

    done

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,27 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// My solution:
+// function capitalize(str) {
+//     const arr = [];
+//     for ( let x of str.split(" ")) {
+//         arr.push(x[0].toUpperCase() + x.slice(1));
+//     }
+//     return arr.join(" ")
+// }
 
+// Solution 2: check the left space is empty
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+    for ( let i = 1; i < str.length; i++){
+        str[i - 1] === ' ' ? result += str[i].toUpperCase() : result += str[i];
+    }
+    return result;
+}
+
+
+
+console.log(capitalize('a short sentence'));
+console.log(capitalize('thee'));
+console.log(capitalize('look, it is working!'));
 module.exports = capitalize;
