a2b408a48fbf1730d9588640be1df8e1189f5bd6
shanx2 <shanshan.cao@live.nl>
2018-08-20 15:33:04 +0200

    done

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,42 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
 
+// My solution 1: Use regular expression
+// function vowels(str) {
+//     let count = str.match(/[aeiou]/gi);
+//     return count === null? 0 : count.length
+// }
+
+// My solution 2: Use for loop
+// function vowels(str) {
+//     let count = 0;
+//     const arr = str.toLowerCase().split('');
+//     for (let i = 0; i < arr.length; i++ ){
+//         if (arr[i] === "i" || arr[i] === "a" || arr[i] === "e" || arr[i] === "o" || arr[i] === "u"){
+//             count += 1;
+//         }
+//     }
+//     return count;
+// }
+
+// Instructor's solution: use str.includes()
+function vowels(str) {
+    let count = 0;
+    const checker = 'aeiou';
+
+    for ( let i of str.toLowerCase()){
+        if (checker.includes(i)){                       //includes also works on Array
+            count += 1;
+        }};
+    return count;
+}
+
+
+
+
+
+console.log(vowels('Hi There!'));
+console.log(vowels('Why do you ask?'));
+console.log(vowels('Why?'));
 module.exports = vowels;
