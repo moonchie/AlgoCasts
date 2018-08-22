831f84ade97eca5041ca3aa046f19a2d01078361
shanx2 <shanshan.cao@live.nl>
2018-08-22 14:27:32 +0200

    a small update

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,53 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// ----- 1. Use while loop
+function fib(num){
+    var a = 1, b = 0, temp;
+
+    while (num >= 0){
+      temp = a;
+      a = a + b;
+      b = temp;
+      num--;
+    }
+    return b;
+  }
+
+console.log(fib(4));
+
+// --------2. recursive method from the internet
+function fibonacci(num) {
+    if( num <= 1) return num;
+
+    return fibonacci(num - 1) + fibonacci(num - 2)
+  }
+ console.log(fibonacci(4))
+
+// --------3. use for loop
+function fibSolution1(n) {
+    const result = [0, 1];
+
+    for (let i = 2; i <= n; i++){
+        let a = result[i - 1];
+        let b = result[i - 2];
+
+        result.push( a + b);
+    }
+    return result[n];
+}
+
+console.log(fibSolution1(4));
+
+// ---------4. use recursive method
+
+
+
+
+
+
+
+
+
 
 module.exports = fib;
