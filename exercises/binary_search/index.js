f6e267c1123ca8118690ec41c33d32bc42785315
shanx2 <shanshan.cao@live.nl>
2018-08-27 17:56:21 +0200

    done

---
+++ exercises/binary_search/index.js
@@ -0,0 +1,20 @@
+// Pseudocode
+function binarySearch(arr, elem) {
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor((start + end) / 2);
+
+    while(arr[middle] !== elem){
+        if (elem < arr[middle]) end = middle - 1;
+        else start = middle + 1;
+        // update the middle:
+        middle = Math.floor((start + end) / 2);
+    }
+    // console.log(start, end, middle);
+    return arr[middle] === elem ? middle : -1;
+
+}
+
+
+// console.log(binarySearch([1,2,3,4,5,6,7,8,10,28], 28));
+console.log(binarySearch([1,2,3,4,5,6,7,8,10,28], 5));
